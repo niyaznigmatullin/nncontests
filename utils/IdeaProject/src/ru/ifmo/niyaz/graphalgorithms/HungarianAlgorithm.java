6e27122355304793997ffee983c9a7b7738b8c79
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2017-03-05 15:36:40 +0300

    hungarian algorithm for long values added

--- utils/IdeaProject/src/ru/ifmo/niyaz/graphalgorithms/HungarianAlgorithm.java
+++ utils/IdeaProject/src/ru/ifmo/niyaz/graphalgorithms/HungarianAlgorithm.java
@@ -79,4 +79,74 @@ public class HungarianAlgorithm {
         }
         return p1;
     }
+
+    static public int[] getMatching(long[][] a) {
+        int n = a.length;
+        int[] p1 = new int[n];
+        int[] p2 = new int[n];
+        long[] row = new long[n];
+        long[] col = new long[n];
+        long allMin = Long.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                allMin = Math.min(allMin, a[i][j]);
+            }
+        }
+        Arrays.fill(row, allMin);
+        Arrays.fill(p1, -1);
+        Arrays.fill(p2, -1);
+        for (int i = 0; i < n; i++) {
+            if (p1[i] >= 0) {
+                continue;
+            }
+            boolean[] was = new boolean[n];
+            int[] from = new int[n];
+            long[] min = new long[n];
+            Arrays.fill(min, Long.MAX_VALUE);
+            int cur = i;
+            int curPair = -1;
+            do {
+                if (curPair >= 0) {
+                    was[curPair] = true;
+                    cur = p2[curPair];
+                }
+                long d = Long.MAX_VALUE;
+                int minPair = -1;
+                long[] aCur = a[cur];
+                for (int j = 0; j < n; j++) {
+                    if (was[j]) {
+                        continue;
+                    }
+                    long val = aCur[j] - row[cur] - col[j];
+                    if (val < min[j]) {
+                        min[j] = val;
+                        from[j] = curPair;
+                    }
+                    if (min[j] < d) {
+                        d = min[j];
+                        minPair = j;
+                    }
+                }
+                row[i] += d;
+                for (int j = 0; j < n; j++) {
+                    if (was[j]) {
+                        col[j] -= d;
+                        row[p2[j]] += d;
+                    } else {
+                        min[j] -= d;
+                    }
+                }
+                curPair = minPair;
+            } while (p2[curPair] >= 0);
+            while (from[curPair] >= 0) {
+                int prev = from[curPair];
+                p2[curPair] = p2[prev];
+                p1[p2[prev]] = curPair;
+                curPair = prev;
+            }
+            p2[curPair] = i;
+            p1[i] = curPair;
+        }
+        return p1;
+    }
 }
